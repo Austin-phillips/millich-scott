242a25b0034e31fc904bce746a3507ee09954783
Austin Phillips <phillips.austin51@gmail.com>
2018-11-07 22:16:35 -0700

    Initial Commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Header, Segment } from 'semantic-ui-react';
+
+class Home extends Component {
+
+  render() {
+    return (
+    <Segment>
+      <Header as='h1' textAlign='center'>Financial Services</Header>
+    </Segment>
+      
+    );
+  }
+}
+
+export default Home;
